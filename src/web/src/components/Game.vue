ec21f4c9176df9fda38eabe0c4d0d06d79bab830
giannis <john_z94@hotmail.com>
2020-07-08 15:48:52 +0300

    remove svg-map library + code

--- src/web/src/components/Game.vue
+++ src/web/src/components/Game.vue
@@ -1,9 +1,5 @@
 <template>
  <div>
-	<!--svg-map id= "map" :map="Japan" /-->
-  <!--div id='mapContainer' style="width: 55%;float:right;">
-    <checkbox-svg-map v-model="selectedLocations" :map="Japan" />
-  </div-->
   <h2 class="text-center"><b>Choose what you want to do!</b></h2>
   <div class="container" style="margin-top:5%">
    <div class="row">
@@ -43,21 +39,16 @@
 </template>
 
 <script>
-//import { SvgMap, CheckboxSvgMap } from "vue-svg-map";
-//import Japan from "@svg-maps/japan";
 import AvatarModal from '@/components/modals/AvatarModal';
 
 export default {
   name: 'Game',
   components: {
       AvatarModal
-  		//SvgMap, CheckboxSvgMap
   	},
   	data() {
   		return {
         showModal: false,
-  			//Japan,
-        //selectedLocations: []
   		};
   	},
     methods: {
@@ -70,21 +61,3 @@ export default {
     }
 }
 </script>
-
-<style>
-.svg-map {
-  width: 100%;
-
-  stroke: #666;
-  stroke-width: 1;
-  stroke-linecap: round;
-  stroke-linejoin: round; }
-  .svg-map__location {
-    fill: #a1d99b;
-    cursor: pointer; }
-    .svg-map__location:focus, .svg-map__location:hover {
-      fill: #b8e2b3;
-      outline: 0; }
-    .svg-map__location[aria-checked="true"] {
-      fill: #f4bc44; }
-</style>
