64422a91fc9221de4b64fbc75927d4398e56b74c
giannis <john_z94@hotmail.com>
2020-09-20 19:47:37 +0300

    pokemon info modal

--- src/web/src/components/modals/PokemonDetails.vue
+++ src/web/src/components/modals/PokemonDetails.vue
@@ -1,14 +1,30 @@
 <template>
-  <Modal>
+  <Modal :width="'400px'">
     <span slot = "close" id = 'closeSymbol' @click.prevent = "close">x</span><br>
     <h3 slot = "header">Pokemon Details</h3>
     <div slot = "body">
-<div class="container-sm">
-      {{info.forms[0].name.charAt(0).toUpperCase()+info.forms[0].name.slice(1)}}
-  </div>
-      <img :src="image" class="pokeImg">
-      {{info.color}}
-
+      <b>{{info.forms[0].name.charAt(0).toUpperCase()+info.forms[0].name.slice(1)}}</b>
+      <div :style="getTypeStyle()">
+       {{ info.types[0].type.name }}
+      </div>
+      <img :src="image" class="pokeImg row" style="margin-top: 10%">
+      <div class = "row"
+           v-for = '(row,ind) in Object.keys(info.stats).length/2'
+           :key = "ind">
+        <div class = "col-md-6" v-if = "info.stats[ind*2]">
+           {{ normalizeStat(info.stats[ind*2].stat.name) }}:<span :style="getTypeStyle()">{{ info.stats[ind*2].base_stat}}</span>
+        </div>
+        <div class = "col-md-6" v-if = "info.stats[ind*2 + 1]">
+           {{ normalizeStat(info.stats[ind*2 + 1].stat.name) }}:<span :style="getTypeStyle()">{{ info.stats[ind*2 + 1].base_stat}}</span>
+        </div>
+      </div>
+    </div>
+    <div slot = "footer" class="text-center">
+     <div :style="getStyle()"
+         v-for="(move,index) in info.moves"
+         v-if="index < 4">
+         {{ move.move.name }}
+     </div>
     </div>
   </Modal>
 </template>
@@ -33,11 +49,18 @@ import pokemonMixin from '@/common/mixins/pokemonMixin';
         this.image = this.getPokemonImage(this.info.id);
       },
       methods: {
-
+        normalizeStat(stat) {
+          return stat.includes('-') ? stat.substring(0, 2).toUpperCase() + stat.substring(stat.indexOf('-')) : stat;
+        },
+        getTypeStyle() {
+          return { 'color' : this.info.color };
+        },
+        getStyle() {
+          return { 'background-color' : 'lightgray', 'color' : this.info.color, 'border-radius' : '2px', 'width' : '50%', 'margin' : '0 auto 2%' };
+        },
         close() {
           this.$emit('close');
         },
-
       },
   }
 </script>
