9207f22b02b93d546f63cae405f8db393a4cc303
giannis <john_z94@hotmail.com>
2020-09-21 21:54:36 +0300

    inventory UI fix

--- src/web/src/components/Inventory.vue
+++ src/web/src/components/Inventory.vue
@@ -3,14 +3,14 @@
     <h1 style="margin-bottom:5%"><img src="../assets/bag.png" height="70px" width="70px"><b>Inventory</b></h1>
    <div class="row" style="width: 100%;margin-top:4%" v-if="stones.length">
      <h3><b>Evolution Stones</b></h3>
-     <div v-for="(stone, index) in stones" :key="index" :class="['col-md-' + 12/stones.length]" style="margin-top:4%">
+     <div v-for="(stone, index) in stones" :key="index" :class="['col-md-' + Math.ceil(12/stones.length)]" style="margin-top:4%">
        <img :src="stone.image" height="70" width="70">
        <h4>{{ stone.name }}</h4>
      </div>
    </div>
    <div class="row" style="width: 100%;margin-top:4%" v-if="items.length">
      <h3><b>Items</b></h3>
-     <div v-for="(item, index) in items" :key="index" :class="['col-md-' + 12/items.length]" style="margin-top:4%">
+     <div v-for="(item, index) in items" :key="index" :class="['col-md-' + Math.ceil(12/items.length)]" style="margin-top:4%">
        <img :src="item.image" height="70" width="70">
        <h4>{{ item.name }}</h4>
      </div>
