dd4de6d6927ba1cc51c83346c2e82c77383c2f4a
Giannis Zagarelos <13730376+jzProg@users.noreply.github.com>
2025-08-21 18:46:12 +0300

    Update firebaseConfigProperties.js

--- src/web/src/common/firebaseConfigProperties.js
+++ src/web/src/common/firebaseConfigProperties.js
@@ -2,13 +2,12 @@ const firebaseConfigProperties = {
   data() {
     return {
       config: {
-        apiKey: "AIzaSyA3CGlkhZxtmiJoSIeLDeGw0NAHCoc9jMI",
-        authDomain: "poke-collect-app.firebaseapp.com",
-        databaseURL: "https://poke-collect-app-default-rtdb.firebaseio.com",
-        projectId: "poke-collect-app",
-        storageBucket: "poke-collect-app.firebasestorage.app",
-        messagingSenderId: "851921713358",
-        appId: "1:851921713358:web:3a1dbc882ca1ae5f7c389f"
+        apiKey: "AIzaSyCpKsZ_d0CjzxTFOy0OKbumJjKqi8y6ORA",
+        authDomain: "pokecollectapp-88362.firebaseapp.com",
+        projectId: "pokecollectapp-88362",
+        storageBucket: "pokecollectapp-88362.firebasestorage.app",
+        messagingSenderId: "227462813402",
+        appId: "1:227462813402:web:0b23604b3bb6571612f7e3"
 
       }
     }
